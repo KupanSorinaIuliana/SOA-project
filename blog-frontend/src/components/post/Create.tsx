b7579225e9d9bbaf79c627b75641ba5d36f1a52f
KupanSorinaIuliana <sorinakupan3@gmail.com>
2023-02-09 09:11:27 +0200

    remove: comments

--- blog-frontend/src/components/post/Create.tsx
+++ blog-frontend/src/components/post/Create.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { withRouter, useHistory } from 'react-router-dom';
-//import { useAuth0 } from '../../contexts/auth0-context';
 import { useAuth0 } from "@auth0/auth0-react";
 
 function Create(): JSX.Element {
--- blog-frontend/src/index.tsx
+++ blog-frontend/src/index.tsx
@@ -2,18 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-//import { Auth0Provider } from './contexts/auth0-context';
 import { Auth0Provider } from "@auth0/auth0-react";
-//import { BrowserRouter } from 'react-router-dom';
-
-/* ReactDOM.render(
-<Auth0Provider>
-    <BrowserRouter>
-    			<App />
-   </BrowserRouter>,
-   </Auth0Provider>,
-	document.getElementById('root')
-); */
 
 ReactDOM.render(
   <Auth0Provider
